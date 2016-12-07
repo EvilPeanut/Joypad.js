be5216c9c2610cf7680c692c283b364abe641447
Reece Lecrivain <reece.lecrivain@rdcstudiosonline.net>
2016-12-07 21:10:51 +0000

    Create the bones

---
+++ Joypad.js
@@ -0,0 +1,20 @@
+/**
+ * @author Reece Aaron Lecrivain / http://reecenotes.com/
+ */
+var JOYPAD = {};
+
+JOYPAD.addInput = function () {
+
+}
+
+JOYPAD.mapInput = function () {
+
+}
+
+JOYPAD.KEYBOARD = {
+
+}
+
+JOYPAD.XBOX = {
+
+}
