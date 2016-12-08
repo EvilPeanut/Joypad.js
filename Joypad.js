ad4334105b134dce49f0a9c862826c4a139f935d
EvilPeanut <reece@amentrix.com>
2016-12-08 11:04:11 +0000

    Add support for common keys

--- Joypad.js
+++ Joypad.js
@@ -28,6 +28,29 @@ $( document ).keyup(function( event ) {
 });
 
 JOYPAD.KEYBOARD = {
+	BACKSPACE : 8,
+	TAB : 9,
+	ENTER : 13,
+	SHIFT : 16,
+	CTRL : 17,
+	ALT : 18,
+	CAPSLOCK : 20,
+	ESCAPE : 27,
+	SPACE : 32,
+	LEFT : 37,
+	UP : 38,
+	RIGHT : 39,
+	DOWN : 40,
+	NUM0 : 48,
+	NUM1 : 49,
+	NUM2 : 50,
+	NUM3 : 51,
+	NUM4 : 52,
+	NUM5 : 53,
+	NUM6 : 54,
+	NUM7 : 55,
+	NUM8 : 56,
+	NUM9 : 57,
 	A : 65,
 	B : 66,
 	C : 67,
@@ -53,7 +76,20 @@ JOYPAD.KEYBOARD = {
 	W : 87,
 	X : 88,
 	Y : 89,
-	Z : 90
+	Z : 90,
+	LEFT_SYSTEM : 91,
+	RIGHT_SYSTEM : 93,
+	COLON : 186,
+	EQUAL : 187,
+	COMMA : 188,
+	DASH : 189,
+	PERIOD : 190,
+	FORWARD_SLASH : 191,
+	ACCENT : 192,
+	OPEN_BRACKET : 219,
+	BACK_SLASH : 220,
+	CLOSE_BRACKET : 221,
+	QUOTE : 222
 }
 
 JOYPAD.XBOX = {
